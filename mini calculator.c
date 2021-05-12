a637bb11101497505d12f19aed095a04368aae96
LEANA18 <83947709+LEANA18@users.noreply.github.com>
2021-05-12 23:08:05 +0530

    Add files via upload

---
+++ mini calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int a,b;
+scanf("%d",&a);
+scanf("%d",&b);
+printf("%d",a+b);
+printf("\n%d",a-b);
+printf("\n%d",a*b);
+printf("\n%.2f",a/(float)b);
+
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
---
+++ square root.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    double a, squareroot;
+    printf("");
+    scanf("%lf",&a);
+
+    squareroot = sqrt(a);
+    printf("%.1lf",squareroot);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
