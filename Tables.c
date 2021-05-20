b2292ac55fb77a7a0372e883056b7e9cb5a1f0c6
LEANA18 <83947709+LEANA18@users.noreply.github.com>
2021-05-20 23:00:01 +0530

    Create Tables.c

---
+++ Tables.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+   do
+   {
+       int i,n;
+       printf("\n1.Table of 1 \n2.Table of 2 \n3.Table of 3 \n4.Table of 4 \n5.Table of 5 \n6.Exit \n");
+       scanf("%d", &n);
+       if(n==6)
+       {
+           exit(0);
+       }
+       if(n>6)
+       {
+           printf("Enter a valid option!");
+       }
+       if(n<6)
+       {
+            for(i=1;i<=10;i++)
+            {
+              printf("%d ",n*i);
+            }
+       }
+   }while(1);
+}
